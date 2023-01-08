c6f64d43a42befec719070fbdc2e92488965b50e
Hazique Quazi <hazique.q@gmail.com>
2023-01-08 13:49:03 -0500

    Backend wrapping Pexels API

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const next = require('next');
+
+const {createClient} = require('pexels');
+const pexelsClient = createClient('563492ad6f917000010000012a940a3e6872471ab80533d31217887f');
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare()
+  .then(() => {
+    const server = express();
+
+    server.get('/api/photos/curated', async (req, res) => {
+      
+      const curatedPics = await pexelsClient.photos.curated({per_page: 10});
+      return res.send({ curated: curatedPics});
+    });
+
+    server.get('/api/photos/search', async (req, res) => {
+      const query = "Nature";
+      const curatedPics = await pexelsClient.photos.search({query, per_page: 10});
+      return res.send({ curated: curatedPics});
+    });
+
+    server.get('*', (req, res) => {
+      return handle(req, res)
+    });
+
+    server.listen(3000, (err) => {
+      if (err) throw err;
+      console.log('> Ready on http://localhost:3000');
+    });
+  })
+  .catch((ex) => {
+    console.error(ex.stack);
+    process.exit(1);
+  })
