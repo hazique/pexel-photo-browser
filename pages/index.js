f812b704eaff28c1e6e20d839e58c0d619b77ddf
Hazique Quazi <hazique.q@gmail.com>
2023-01-26 13:12:11 -0500

    Page reload to keep render instance running

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
-import Head from 'next/head';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 
 import ImageBrowser from '../components/imageBrowser';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import Navbar from '../components/navBar';
 import Search from '../components/search';
@@ -10,6 +11,16 @@ import Search from '../components/search';
 export default function App() {
 
   const [searchString, setSearchString] = useState(null);
+  const router = useRouter();
+
+  useEffect(()=>{
+    const reloadInterval = window.setInterval(()=>{
+      console.log("reloading page");
+      router.reload();
+    }, 1000 * 60 * 2);
+
+  return ()=> window.clearInterval(reloadInterval);
+  }, []);
 
   return (
     <div>
